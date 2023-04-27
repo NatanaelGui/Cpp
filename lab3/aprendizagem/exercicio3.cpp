44bea9ee1173a89c24be020179a648f73c794c25
Luiz Santana <egi@wi.com.br>
2023-04-27 10:36:01 -0300

    exercicio de aprendizagem 3

--- lab3/aprendizagem/exercicio3.cpp
+++ lab3/aprendizagem/exercicio3.cpp
@@ -3,17 +3,32 @@
 using namespace std;
 int main (void)
 {	
-	int numero = 0, i = 0, multiplicado = 0, resultado = 0;
-	cout << "Entre com um número de (0 a 9): ";
-	cin >> numero;
-	cout << "Tabuada do " << numero << endl;
-	cout << "----------------" << endl;
-	for (i > 0; i <= 9; i++)
-	{
-		resultado = numero * multiplicado;
-		cout << numero << " x " << multiplicado << " = " << resultado << endl;
-		multiplicado = multiplicado + 1;
-	}
-	cout << endl;
+	float tempoM = 0, tempoH = 0, quilos = 0, tempoCorrida = 0, tempoCiclismo = 0, tempoNatacao;
+	float calorias = 0;
+	char separador = ' ';
+	cout << "Digite seu peso em quilos: ";
+	cin >> quilos;
+
+	cout << "Digite o tempo de corrida: ";
+	cin >> tempoH >> separador >> tempoM >> separador;
+	tempoH = tempoH * 60;
+	tempoCorrida = tempoH + tempoM;
+
+	cout << "Digite o tempo de ciclismo: ";
+	cin >> tempoH >> separador >> tempoM >> separador;
+	tempoH = tempoH * 60;
+	tempoCiclismo = tempoH + tempoM;
+
+	cout << "Digite o tempo de natação: ";
+	cin >> tempoH >> separador >> tempoM >> separador;
+	tempoH = tempoH * 60;
+	tempoNatacao = tempoH + tempoM;
+
+	calorias += 7.0 * quilos * (tempoCorrida / 60);
+	calorias += 7.0 * quilos * (tempoCiclismo / 60);
+	calorias += 8.0 * quilos * (tempoNatacao / 60);
+
+	cout << "Você gastou um total de " << calorias << " calorias." << endl;
+
 	return 0;
 }
