3884828ed27d74d73f7d5b32d40b8c865fb9f491
NatanaelGui <natanael1523@gmail.com>
2025-08-16 21:51:21 -0300

    lab 18 concluido

---
+++ lab18/revisao/exercicio3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;    
+
+    while(n != 7){
+        cout << "Digite um numero entre 0-10: ";
+        cin >> n;
+    }
+
+    cout << "Sim! 7 eh meu numero favorito.\n";
+    return 0;
+}
